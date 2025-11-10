8af4686d852083f439804e74903e399e3aba0560
takero8538-lgtm <takero8538@gmail.com>
2025-11-10 23:36:53 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,35 @@
+const CACHE_NAME = "medicine-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// インストール時にキャッシュ作成
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// 新しいファイルがあれば更新
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// ネット優先で取得、失敗したらキャッシュ
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
